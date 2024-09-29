2f20bfef74ad37267a562fe6f99b72998d092344
Fernando Perez <fernandohps17@gmail.com>
2024-09-28 23:57:00 -0400

    Se arreglo error en el footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,10 +6,12 @@ const Footer = () => {
     const { filters } = useFilters()
     // 1h y 5m
 
+
   return (
     <footer className='footer'>
       <h4>Prueba técnica de React ⭐ - <span>@midudev</span></h4>
       <h5>Shopping Cart con useContext & useReducer</h5>
+      <h3>hola nuevamente</h3>
     </footer>
   )
 }
