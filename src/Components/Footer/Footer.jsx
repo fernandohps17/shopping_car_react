f799f212bbaeacab3013c75cb46937935dc3b26a
Fernando Perez <fernandohps17@gmail.com>
2024-10-02 18:38:06 -0400

    Se creo el contexto del carrito

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,20 @@
+import { useCart } from '../../Hook/useCart'
+import { useFilters } from '../../Hook/useFilters'
 import './Footer.scss'
 
 const Footer = () => {
 
+  const { filters } = useFilters()
+  const { cart } = useCart()
+
   return (
     <footer className='footer'>
+      {
+        JSON.stringify(filters, null, 2)
+      }
+      {
+        JSON.stringify(cart, null, 2)
+      }
       <h4>Prueba técnica de React ⭐ - <span>@midudev</span></h4>
       <h5>Shopping Cart con useContext & useReducer</h5>
     </footer>
--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -7,30 +7,34 @@ const Products = ({ products }) => {
 
   const { addToCart, cart } = useCart()
 
+  const checkProductInCart = product => {
+    return cart.some(item => item.id === product.id)
+  }
+
 
   return (
     <main className='products'>
       <ul>
         {
-            products.slice(0, 10).map(product => (
-                <li key={product.id}>
-                    <img src={product.thumbnail} alt={product.title} />
+          products.slice(0, 10).map(product => (
+            <li key={product.id} >
+              <img src={product.thumbnail} alt={product.title} />
 
-                    <div>
-                        <strong>{product.title}</strong> - ${product.price}
-                    </div>
+              <div>
+                <strong>{product.title}</strong> - ${product.price}
+              </div>
 
-                    <div>
-                        <button>
-                            <AddToCartIcon />
-                        </button>
-                    </div>
-                </li>
-            ))
+              <div>
+                <button>
+                  <AddToCartIcon />
+                </button>
+              </div>
+            </li>
+          ))
         }
       </ul>
-    </main>
+    </main >
   )
 }
-
+// 1h 27m 40s video midulive tienda y carrito con react
 export default Products
